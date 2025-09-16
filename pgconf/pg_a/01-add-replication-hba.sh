0282468b1dfea6b5042fff66afd8f5da5c1b31cf
Rameev Timur <stt1xx@mail.ru>
2025-09-16 03:10:54 +0300

    made pg_b sync

--- pgconf/pg_a/01-add-replication-hba.sh
+++ pgconf/pg_a/01-add-replication-hba.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-  grep -q "host    replication     $REPLICATION_USER" $PGDATA/pg_hba.conf || \
-  echo "host    replication     $REPLICATION_USER      all         trust" >> $PGDATA/pg_hba.conf
+# settings to pg_hba.conf
+grep -q "host    replication     $REPLICATION_USER" $PGDATA/pg_hba.conf || \
+echo "host    replication     $REPLICATION_USER      all         trust" >> $PGDATA/pg_hba.conf
+# settings to postgresql.conf
+cat /docker-entrypoint-initdb.d/sync.conf >> "$PGDATA/postgresql.conf"
