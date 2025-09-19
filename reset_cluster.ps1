ab487164ca0607bb97deb0f99fe196690412f8a6
Rameev Timur <stt1xx@mail.ru>
2025-09-19 11:51:30 +0300

    lab done (full)

--- reset_cluster.ps1
+++ reset_cluster.ps1
@@ -1,23 +1,19 @@
-# Сохраняем путь к файлу
 $composeFile = "docker-compose.yml"
 
-# 1. Закомментировать строку №12 с RESTORE
 (Get-Content $composeFile) -replace '(^\s*#?\s*- RESTORE=.*)', '# $1' |
     Set-Content $composeFile
 
-# 2. Остановить и удалить все контейнеры + volume
 docker compose down -v
-Start-Sleep -Seconds 10
-# 3. Пересобрать образы
+Start-Sleep -Seconds 3
 docker compose build
 Start-Sleep -Seconds 10
-# 4. Запустить все контейнеры
 docker compose up -d
-Start-Sleep -Seconds 12
+Start-Sleep -Seconds 10
 
-
-Start-Sleep -Seconds 7
-docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('bob', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('volodya', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('nicolya', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('bodyan', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "SELECT * FROM accounts;"
 
 
 (Get-Content $composeFile) -replace '^\s*#?\s*- RESTORE=.*', '      - RESTORE=REPLICA' |
@@ -25,15 +21,22 @@ docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO
 
 # 8. Запустить pg-a
 docker compose up -d --force-recreate pg-a
-Start-Sleep -Seconds 7
+Start-Sleep -Seconds 10
 
-Start-Sleep -Seconds 7
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('bob', 1000);"
 docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('pit', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('taras', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('tristan', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "SELECT * FROM accounts;"
 
 (Get-Content $composeFile) -replace '^\s*- RESTORE=.*', '      - RESTORE=PRIMARY' |
     Set-Content $composeFile
 
 docker compose up -d --force-recreate pg-a
-Start-Sleep -Seconds 15
+Start-Sleep -Seconds 10
 
-docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('taras', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('aaaaaa', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('bbbbbb', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('cccccc', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "INSERT INTO accounts(name, balance) VALUES ('dddddd', 1000);"
+docker exec pg-client psql -h pgpool -p 9999 -U postgres -d demo -c "SELECT * FROM accounts;"
